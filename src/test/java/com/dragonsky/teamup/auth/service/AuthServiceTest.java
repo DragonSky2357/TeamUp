e0514be7ac2047fe207f65c0452636f94b753c2f
DragonSky <38320524+DragonSky2357@users.noreply.github.com>
2024-06-12 12:29:14 +0900

    test: 회원가입, refresh token 테스트 코드 추가

--- src/test/java/com/dragonsky/teamup/auth/service/AuthServiceTest.java
+++ src/test/java/com/dragonsky/teamup/auth/service/AuthServiceTest.java
@@ -1,101 +1,108 @@
 package com.dragonsky.teamup.auth.service;
 
 import com.dragonsky.teamup.auth.dto.request.SignupRequest;
-import com.dragonsky.teamup.auth.exception.ErrorCode;
 import com.dragonsky.teamup.auth.exception.MemberException;
-import com.dragonsky.teamup.auth.model.Refresh;
 import com.dragonsky.teamup.auth.repository.RefreshRepository;
+import com.dragonsky.teamup.global.util.cookie.CookieUtil;
 import com.dragonsky.teamup.global.util.jwt.JWTUtil;
 import com.dragonsky.teamup.member.model.Member;
+import com.dragonsky.teamup.member.model.Role;
 import com.dragonsky.teamup.member.repository.MemberRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceTest {
 
-    @Mock
-    private MemberRepository memberRepository;
-
-    @Mock
-    private PasswordEncoder passwordEncoder;
-
-    @Mock
-    private JWTUtil jwtUtil;
-
-    @Mock
-    private RefreshRepository refreshRepository;
-
-    @Mock
-    private HttpServletRequest request;
-
-    @Mock
-    private HttpServletResponse response;
-
-    @InjectMocks
+    @Autowired
     private AuthService authService;
 
-    private SignupRequest signupRequest;
+    @Autowired
+    private MemberRepository memberRepository;
 
-    @BeforeEach
-    public void setUp(){
-        signupRequest = SignupRequest.builder()
-                .email("test@naver.com")
-                .nickname("test")
-                .password("password")
-                .build();
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @MockBean
+    private JWTUtil jwtUtil;
+
+    @MockBean
+    private RefreshRepository refreshRepository;
+
+    @MockBean
+    private CookieUtil cookieUtil;
 
     @Test
     @DisplayName("회원가입 성공")
-    @Transactional
     void signUp() {
-        when(memberRepository.findByEmail(signupRequest.getEmail())).thenReturn(null);
-        when(passwordEncoder.encode(signupRequest.getPassword())).thenReturn("encodedPassword");
-        when(memberRepository.save(any(Member.class))).thenReturn(Member.builder().build());
+        // given
+        SignupRequest signupRequest = SignupRequest.builder()
+                .email("test@naver.com")
+                .password("password")
+                .nickname("nickname")
+                .build();
 
+        // when
         authService.signUp(signupRequest);
 
-        verify(memberRepository,times(1)).findByEmail(signupRequest.getEmail());
-        verify(memberRepository, times(1)).save(any(Member.class));
+        // then
+        Member member = memberRepository.findByEmail(signupRequest.getEmail());
+        assertNotNull(member);
+        assertEquals(signupRequest.getEmail(), member.getEmail());
     }
 
     @Test
-    @DisplayName("회원가입 이메일 중복")
-    public void testSignUp_DuplicateEmail() {
-        Member member = Member.builder().build();
-        when(memberRepository.findByEmail(signupRequest.getEmail())).thenReturn(member);
+    @DisplayName("중복된 이메일 회원 가입 요청")
+    public void testSignUp_DuplicateEmail(){
+        // given
+        Member member = Member.builder()
+                .email("test@naver.com")
+                .nickname("nickname")
+                .password("password")
+                .role(Role.MEMBER)
+                .build();
+        memberRepository.save(member);
 
-        MemberException exception = assertThrows(MemberException.class,() -> authService.signUp(signupRequest));
-        assertEquals(ErrorCode.DUPLICATED_MEMBER.message,exception.getMessage());
-        verify(memberRepository, times(1)).findByEmail(signupRequest.getEmail());
-        verify(memberRepository, never()).save(any(Member.class));
+        SignupRequest signupRequest = SignupRequest.builder()
+                .email("test@naver.com")
+                .password("password")
+                .nickname("nickname")
+                .build();
+
+        // when & then
+        assertThrows(MemberException.class, ()->{
+            authService.signUp(signupRequest);
+        });
     }
 
     @Test
-    public void testReisusu_Success(){
+    public void testReissue(){
         // Given
-        Cookie[] cookies = {new Cookie("refresh", "validRefreshToken")};
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        Cookie refreshCookie  = new Cookie("refresh","testRefreshToken");
+        Cookie[] cookies = {refreshCookie};
+
         when(request.getCookies()).thenReturn(cookies);
-        when(jwtUtil.isExpired("validRefreshToken")).thenReturn(false);
-        when(jwtUtil.getCategory("validRefreshToken")).thenReturn("refresh");
-        when(jwtUtil.getId("validRefreshToken")).thenReturn(1L);
-        when(jwtUtil.getUsername("validRefreshToken")).thenReturn("testUser");
-        when(jwtUtil.getRole("validRefreshToken")).thenReturn("ROLE_USER");
+        when(jwtUtil.isExpired(anyString())).thenReturn(false);
+        when(jwtUtil.getCategory(anyString())).thenReturn("refresh");
+        when(jwtUtil.getId(anyString())).thenReturn(1L);
+        when(jwtUtil.getUsername(anyString())).thenReturn("testUser");
+        when(jwtUtil.getRole(anyString())).thenReturn("ROLE_MEMBER");
         when(jwtUtil.createJwt(anyString(), anyLong(), anyString(), anyString())).thenReturn("newAccessToken");
         when(jwtUtil.createRefresh(anyString())).thenReturn("newRefreshToken");
 
@@ -103,8 +110,7 @@ class AuthServiceTest {
         authService.reissue(request, response);
 
         // Then
+        verify(refreshRepository, times(1)).deleteByRefresh(anyString());
         verify(response, times(2)).addCookie(any(Cookie.class));
-        verify(refreshRepository, times(1)).deleteByRefresh("validRefreshToken");
-        verify(refreshRepository, times(1)).save(any(Refresh.class));
     }
 }
